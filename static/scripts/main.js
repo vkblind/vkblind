dfb80245f9e84b87774b2a8ea9da5f7f7fe57d8e
Sergey Chipiga <svchipiga@yandex-team.ru>
2014-09-14 16:54:48 +0400

    remove invert a

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,26 +5,21 @@ require([
 function ($) {
 
     html = $("html");
-    a = $("a");
 
     $(document).ready(function(){
         if (localStorage.getItem("is_inverted") == "true"){
             html.addClass("inverted");
-            a.addClass("inverted");
         } else {
             html.removeClass("inverted");
-            a.removeClass("inverted");
         }
     });
 
     $("#invert").click(function(){
         if (html.hasClass("inverted") && a.hasClass("inverted")) {
             html.removeClass("inverted");
-            a.removeClass("inverted");
             localStorage.setItem("is_inverted", false);
         } else {
             html.addClass("inverted");
-            a.addClass("inverted");
             localStorage.setItem("is_inverted", true);
         }
     });
