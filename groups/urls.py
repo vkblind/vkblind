97245fa4a61b26e98f245451d0c17579945a3b26
Sergey Chipiga <chipiga86@gmail.com>
2014-09-30 23:52:22 +0400

    add search form

--- groups/urls.py
+++ groups/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import patterns, include, url
 
-from views import view_groups, view_group
+from views import view_groups, view_group, search_groups
 
 
 urlpatterns = patterns('',
     url(r'^$', view_groups, name='view_groups'),
     url(r'^(\d+)/$', view_group, name='view_group'),
+    url(r'^search-groups/$', search_groups, name='search_groups'),
 )
