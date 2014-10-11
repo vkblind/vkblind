6675115cd9adaf29238851e39192d49434c3e583
Sergey Chipiga <svchipiga@yandex-team.ru>
2014-10-11 22:29:18 +0400

    Format urls

--- groups/urls.py
+++ groups/urls.py
@@ -2,11 +2,10 @@
 
 from django.conf.urls import patterns, include, url
 
-from views import view_groups, view_group, search_groups
+from views import view_groups, view_group
 
 
 urlpatterns = patterns('',
     url(r'^$', view_groups, name='view_groups'),
-    url(r'^(\d+)/$', view_group, name='view_group'),
-    url(r'^search-groups/$', search_groups, name='search_groups'),
+    url(r'^(\d+)$', view_group, name='view_group')
 )
--- search/views.py
+++ search/views.py
@@ -7,16 +7,19 @@ from annoying.decorators import render_to
 from vkblind.decorators import vk_api
 
 
+VK_HOST = "https://vk.com"
+ITEMS_COUNT = 20
+
 @vk_api
 @render_to('search.html')
 def search(request):
-    groups = None
-    users = None
+    vk_url = users = groups = None
 
-    if request.method == 'POST':
-        query = request.POST.get('query')
-        if query:
-            groups = request.vk.groups.search(q=query, count=10)['items']
-            users = request.vk.users.search(q=query, count=10)['items']
+    query = request.REQUEST.get('c[q]')
+    if query:
+        groups = request.vk.groups.search(q=query, count=ITEMS_COUNT)['items']
+        users = request.vk.users.search(q=query, count=ITEMS_COUNT)['items']
 
-    return {'groups': groups or [], 'users': users or []}
+        vk_url = VK_HOST + request.get_full_path()
+
+    return {'groups': groups or [], 'users': users or [], 'vk_url': vk_url}
