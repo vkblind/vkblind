e08ce120a5ac1d8c864586bc846bf265c927ee7c
Timur Zaripov <q210@yandex-team.ru>
2014-10-11 20:55:42 +0400

    using django forms for user settings saving. Also user settings now in one-to-one relation to user model

---
+++ vkblind/user_settings/migrations/0002_auto_20141011_1652.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user_settings', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='usersettings',
+            name='color_scheme',
+            field=models.CharField(default=b'black', max_length=20, choices=[(b'black', b'black'), (b'white', b'white')]),
+        ),
+        migrations.AlterField(
+            model_name='usersettings',
+            name='font_size',
+            field=models.CharField(default=b'L', max_length=5, choices=[(b'L', b'large'), (b'M', b'medium'), (b'S', b'small')]),
+        ),
+        migrations.AlterField(
+            model_name='usersettings',
+            name='user',
+            field=models.OneToOneField(related_name=b'settings', to=settings.AUTH_USER_MODEL),
+        ),
+    ]
--- vkblind/user_settings/views.py
+++ vkblind/user_settings/views.py
@@ -1,23 +1,31 @@
 # coding: utf-8
 
 from annoying.decorators import render_to
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from . import models
+from django.forms.models import model_to_dict
+
+from .models import UserSettings
+from .forms import UserSettingsForm
 
 
 @render_to('settings.html')
 def settings(request):
-    return {}
+    """
+    View user settings page with color/font-size options form
+    """
+    settings_inst, created = UserSettings.objects.get_or_create(user=request.user)
+    return {
+        'settings': model_to_dict(settings_inst)
+    }
 
 
 def save_settings(request):
-    user_data = {
-        'font_size': request.POST['font_size'],
-        'color_scheme': request.POST['color_scheme'],
-    }
-    user_settings, created = models.UserSettings.objects.get_or_create(
-        user=request.user, defaults=user_data)
-    if not created:
-        models.UserSettings.objects.filter(
-            user=request.user).update(**user_data)
-    return redirect('../')
+    """
+    Save user settings data from POST request
+    """
+    settings_inst, created = UserSettings.objects.get_or_create(user=request.user)
+
+    form = UserSettingsForm(request.POST, instance=settings_inst)
+    form.save()
+    return redirect(reverse('settings'))
