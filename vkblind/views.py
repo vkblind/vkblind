505c44d3f85c9b2bff3a509ecc6c394b31d4983d
Timur Zaripov <q210@yandex-team.ru>
2014-10-18 21:56:36 +0400

    added user wall posts on profile page

--- vkblind/views.py
+++ vkblind/views.py
@@ -5,12 +5,12 @@ import logging
 from pprintpp import pprint
 
 from django.http import HttpResponseBadRequest
+from django.conf import settings
 from django.contrib.auth import logout as logout_user
 from django.shortcuts import redirect
 from annoying.decorators import render_to
 
 from .decorators import vk_api
-from .utils import user_to_uid
 
 logger = logging.getLogger(__name__)
 
@@ -34,12 +34,17 @@ def profile(request, vkuser):
     """
     try:
         account_data = request.vk.user_profile_data(vkuser)
+        wall_records = request.vk.wall.get(
+            owner_id=account_data['id'], offset=0, count=settings.ITEMS_LIMIT, filter='all'
+        )
     except vk.VkAPIMethodError as exc:
         logger.exception(repr(exc))
         return HttpResponseBadRequest('An error occurred, sorry')
 
+    # pprint(wall_records)
     return {
         'account': account_data,
+        'wall': wall_records
     }
 
 
