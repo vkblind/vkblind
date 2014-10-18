e3467f3a14f06319bc9b7a6280dc86e01d32089c
Timur Zaripov <q210@yandex-team.ru>
2014-10-18 20:32:57 +0400

    added proper tags handling - will search them on vkontakte for now

--- vkblind/helpers.py
+++ vkblind/helpers.py
@@ -7,6 +7,7 @@ from string import punctuation
 
 unreadable_pattern = r'[^\w\s%s]' % re.sub(r'(.)', r'\\\1', punctuation)
 VK_INTERNAL_LINKS = re.compile(r'\[(id|club)(\d*?)\|(.*?)\]')
+VK_TAGS = re.compile(r'#(\w+?)(?:\s|$|\.|,|!|\?)', re.U)
 
 
 def strip_unreadable(text):
@@ -15,7 +16,8 @@ def strip_unreadable(text):
 
 def process_vk_internal_links(text):
     """
-    Преобразовывает линки вида [club123|Club name]
+    Преобразовывает линки вида [club123|Club name] и теги
     """
     result = re.sub(VK_INTERNAL_LINKS, r'<a href="/\1/\2">\3</a>', text)
+    result = re.sub(VK_TAGS, r'<a href="http://vk.com/feed?section=search&q=%23\1">#\1</a>', result)
     return result
